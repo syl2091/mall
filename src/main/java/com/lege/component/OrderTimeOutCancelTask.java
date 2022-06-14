2271c6db7221da3fd4daf5d89cbde1523e25dd7f
onthroad <164378335@qq.com>
2022-06-14 16:59:36 +0800

    mall整合SpringTask

---
+++ src/main/java/com/lege/component/OrderTimeOutCancelTask.java
@@ -0,0 +1,25 @@
+package com.lege.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lege
+ * @Description
+ * @create 2022-06-14 16:53
+ */
+@Component
+public class OrderTimeOutCancelTask {
+    private Logger LOGGER = LoggerFactory.getLogger(OrderTimeOutCancelTask.class);
+    /**
+     * cron表达式：Seconds Minutes Hours DayofMonth Month DayofWeek [Year]
+     * 每10分钟扫描一次，扫描设定超时时间之前下的订单，如果没支付则取消该订单
+     */
+    @Scheduled(cron = "0 0/10 * ? * ?")
+    private void cancelTimeOutOrder() {
+        // TODO: 2022/6/14 此处应调用取消订单的方法，具体查看mall项目源码
+        LOGGER.info("取消订单，并根据sku编号释放锁定库存");
+    }
+}
---
+++ src/main/java/com/lege/config/SpringTaskConfig.java
@@ -0,0 +1,14 @@
+package com.lege.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author lege
+ * @Description 定时任务配置
+ * @create 2022-06-14 16:35
+ */
+@EnableScheduling
+@Configuration
+public class SpringTaskConfig {
+}
